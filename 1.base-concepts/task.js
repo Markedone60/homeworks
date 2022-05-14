d58d7b9547c46e3fc24a3cc00ceb295e43a39cda
Grigory Voronkov <gregory.rvl@yandex.ru>
2022-05-14 19:32:26 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,21 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
-  return arr; // array
+
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr;
+  }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
