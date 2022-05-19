2bd7b997aa3bcd62f494229b790fa3d209a90dbc
Grigory <98592252+Markedone60@users.noreply.github.com>
2022-05-19 16:40:32 +0300

    Initial commit of homework "functions"

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,62 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = 100; 
+  let max = -100;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (i > max) {
+      max = arr[i];
+    } 
+    if (i < min) {
+      min = arr[i];
+    }
 
-  return { min: min, max: max, avg: avg };
+    sum += arr[i];
+  }
+
+  avg = parseFloat((sum / arr.length).toFixed(2));
+
+  return {min: min, max: max, avg: avg};
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
+  let max = -Infinity;
   
+  for (j = 0; j < arrOfArr.length; j++) {
+    if (func(arrOfArr[j]) > max) {
+    max = func(arrOfArr[j]);
+    }
+  }
   return max;
 }
 
 // Задание 3
+
 function worker2(arr) {
-  // Ваш код
+  let max = -Infinity;
+  let min = Infinity;
+  let divider = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    if (i > max) {
+      max = arr[i];
+    } 
+    if (i < min) {
+      min = arr[i];
+    }
+    divider = Math.abs(max - min);
+  }
+
+  return divider;
 }
