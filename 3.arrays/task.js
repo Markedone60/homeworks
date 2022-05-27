e154198c8f026e2a070d3b076ad4d84033d72508
Grigory Voronkov <gregory.rvl@yandex.ru>
2022-05-27 16:24:54 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
+  
+  let lengthComparison = arr1.length === arr2.length; 
+  let indexComparison = arr1.every((item, index) => item === arr2[index]);
+  let result = lengthComparison && indexComparison === true;
 
   return result; // boolean
 }
+
 
 function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
